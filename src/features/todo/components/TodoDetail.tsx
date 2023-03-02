bd9a62a98f7cb97850bff641fd13151014dbc8a2
ryogo-ito <106786725+ryogo-ito@users.noreply.github.com>
2023-03-03 02:49:24 +0900

    テキストの修正

--- src/features/todo/components/TodoDetail.tsx
+++ src/features/todo/components/TodoDetail.tsx
@@ -4,7 +4,7 @@ export function TodoDetail() {
   return (
     <Container maxW="container.xl">
       <VStack>
-        <Text fontSize="6xl">テキスと</Text>
+        <Text fontSize="6xl">テキスト</Text>
         <Textarea />
         <Button colorScheme="teal">保存</Button>
       </VStack>
