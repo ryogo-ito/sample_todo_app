89483bb04fa7b45a546035fb074e88a3b7a2c5d4
ryogo-ito <106786725+ryogo-ito@users.noreply.github.com>
2023-04-03 01:55:58 +0900

    カレンダーのroute追加

---
+++ src/features/calender/components/Calender.tsx
@@ -0,0 +1,3 @@
+export function Calender() {
+  return <div>カレンダー</div>;
+}
--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -1,9 +1,10 @@
-import { Suspense } from "react";
-import { Spinner } from "@chakra-ui/react";
-import { Outlet, RouteObject } from "react-router-dom";
-import { MainLayout } from "../layout/MainLayout";
-import { TaskTable } from "../features/task/components/TaskTable";
-import { TaskDetail } from "../features/task/components/TaskDetail";
+import { Suspense } from 'react';
+import { Spinner } from '@chakra-ui/react';
+import { Outlet, RouteObject } from 'react-router-dom';
+import { MainLayout } from '../layout/MainLayout';
+import { TaskTable } from '../features/task/components/TaskTable';
+import { TaskDetail } from '../features/task/components/TaskDetail';
+import { Calender } from '../features/calender/components/Calender';
 
 function App() {
   return (
@@ -27,14 +28,18 @@ function App() {
 
 export const protectedRoutes: RouteObject[] = [
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <TaskTable />,
       },
-      { path: "/task/:taskId", element: <TaskDetail /> },
+      { path: '/task/:taskId', element: <TaskDetail /> },
     ],
+  },
+  {
+    path: '/calender',
+    element: <Calender />,
   },
 ];
